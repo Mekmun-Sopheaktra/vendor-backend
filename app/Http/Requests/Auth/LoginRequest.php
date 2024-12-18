c7a30e217331e93b8d95db7a203fe4273425aa78
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2024-12-18 22:30:36 +0700

    Update

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -11,13 +11,13 @@ class LoginRequest extends FormRequest
 
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     public function rules(): array
     {
         return [
-            'email' => 'required|max:255|email',
+            'email' => 'required|max:255',
             'password' => 'required',
         ];
     }
