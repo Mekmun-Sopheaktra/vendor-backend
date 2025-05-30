be569b50436f48a98959926dddef3bee0661aaca
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2025-05-30 11:12:33 +0700

    Sail

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cd /var/www/html/api-admin/ && git pull
-cd /var/www/html/api-admin/ && php artisan migrate
-cd /var/www/html/api-admin/ && php artisan optimize:clear
-cd /var/www/html/api-admin/ && ls -al
+cd /var/www/jorngka/vendor-backend/ && git pull
+cd /var/www/jorngka/vendor-backend/ && php artisan migrate
+cd /var/www/jorngka/vendor-backend/ && php artisan optimize:clear
+cd /var/www/jorngka/vendor-backend/ && ls -al
